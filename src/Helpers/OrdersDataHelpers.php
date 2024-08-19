918066a0c377fa87ae7c9dc83c4a5a79d648412b
Adegbenga Agoro <adegbenga@designsbycrenet.com>
2024-08-19 16:17:09 +0100

    chore: introduced fixtures to support running the console command for order processing

--- src/Helpers/OrdersDataHelpers.php
+++ src/Helpers/OrdersDataHelpers.php
@@ -18,4 +18,20 @@ class OrdersDataHelpers
     $randomIndex = array_rand($orderStatuses);
     return $orderStatuses[$randomIndex];
   }
+
+  /**
+   * Retrieves a random delivery option from a predefined list.
+   *
+   * This method selects a random delivery option from an array of possible options,
+   * which includes 'Standard Delivery', 'Express Delivery', 'Same Day Delivery',
+   * and 'Amazon Prime'.
+   *
+   * @return string A randomly selected delivery option from the list.
+   */
+  public static function getRandomDeliveryOption(): string
+  {
+    $deliveryOptions = ['Standard Delivery', 'Express Delivery', 'Same Day Delivery', 'Amazon Prime'];
+    $randomIndex = array_rand($deliveryOptions);
+    return $deliveryOptions[$randomIndex];
+  }
 }
